8d030498103ec712f1f7c3c289fdcb14d188bb57
Alexander <30441441+losica2014@users.noreply.github.com>
2021-01-25 23:08:12 +0300

    Update while-task-tool.py

--- 10/inf/python/while-task-tool.py
+++ 10/inf/python/while-task-tool.py
@@ -1,19 +1,23 @@
 answ = []
 
 for x in range(1, 1000):
-    # print(x)
     xo = x
+    
+    # Start of given program
+    
     a = 0; b = 1
     while x > 0:
         a = a + 1
         b = b * (x % 8)
         x = x // 8
-        # print(a, b)
+        
+    # End of given program
+    
+    # Check for conditions
     if(a == 3) & (b == 24):
-        # print(xo)
         answ.append(xo)
 
-r = 0
+r = answ[0]
 print(answ)
 for i in answ:
     if(i > r):
