c546f04e5c5da53dec4bb58c6da82850c68ee909
Alexander <30441441+losica2014@users.noreply.github.com>
2021-01-29 14:12:09 +0300

    Create while-task-tool.withcomments.py
    WIP

---
+++ 10/inf/python/while-task-tool.withcomments.py
@@ -0,0 +1,25 @@
+answ = [] # Создаём пустой массив
+
+for x in range(1, 1000): # Проводим перебор вводимых значений
+    xo = x # Сохраняем значение x в начале цикла
+    
+    # Start of given program
+    
+    a = 0; b = 1
+    while x > 0:
+        a = a + 1
+        b = b * (x % 8)
+        x = x // 8
+        
+    # End of given program
+    
+    # Check for conditions
+    if(a == 3) & (b == 24):
+        answ.append(xo)
+
+r = answ[0]
+print(answ)
+for i in answ:
+    if(i > r):
+        r = i
+print(r)
