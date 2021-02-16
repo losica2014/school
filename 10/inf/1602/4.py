8988d6b6c1de46625d1bb9d6708b6139db72521f
Alexander <30441441+losica2014@users.noreply.github.com>
2021-02-16 13:34:42 +0300

    Add files via upload

---
+++ 10/inf/1602/4.py
@@ -0,0 +1,9 @@
+def printAge(n):
+    if(n % 10 == 1):
+        print(n, "год")
+    elif(n % 10 >= 2) and (n % 10 < 5):
+        print(n, "года")
+    else:
+        print(n, "лет")
+
+printAge(int(input()))
